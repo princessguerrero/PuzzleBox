72543cc8b11156af3a8e5bd46eaeef779215c621
princessguerrero <princessguerrero@ac.c4q.nyc>
2018-05-11 10:51:18 -0400

    add auth folder

---
+++ backend/auth/passport.js
@@ -0,0 +1,20 @@
+const passport = require("passport");
+const pgp = require("pg-promise")({});
+const db = pgp("postgres://localhost/puzzlebox");
+
+module.exports = () => {
+  passport.serializeUser((user, done) => {
+    done(null, user.username);
+  });
+
+  passport.deserializeUser((username, done) => {
+    db
+      .one("SELECT id, username FROM users WHERE username=$1", [username])
+      .then(user => {
+        return done(null, user);
+      })
+      .catch(err => {
+        return done(err, null);
+      });
+  });
+};
