8aadf20ef0c2a5b647deb0bc5bebd640dc55911d
princessguerrero <princessguerrero@ac.c4q.nyc>
2018-05-14 11:25:36 -0400

    edit sql and add query functions

--- backend/db/puzzlebox.sql
+++ backend/db/puzzlebox.sql
@@ -3,16 +3,24 @@ CREATE DATABASE puzzlebox;
 
 \c puzzlebox;
 
-DROP TABLE IF EXISTS users_main, authorized_users, user_child, contacts, contacts_notes, services, services_notes;
+DROP TABLE IF EXISTS users_main, users_main_bio, authorized_users, user_child, contacts, contacts_notes, services, services_notes;
 
 CREATE TABLE users_main (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR NOT NULL UNIQUE,
+  email VARCHAR NOT NULL UNIQUE,
+  password_digest VARCHAR NOT NULL
+);
+
+CREATE TABLE users_main_bio (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR NOT NULL,
   last_name VARCHAR NOT NULL,
   relationship VARCHAR NOT NULL,
-  username VARCHAR NOT NULL UNIQUE,
-  email VARCHAR NOT NULL UNIQUE,
-  password_digest VARCHAR NOT NULL
+  pic VARCHAR,
+  current_goals VARCHAR,
+  next_steps VARCHAR,
+  notes VARCHAR
 );
 
 CREATE TABLE user_child (
@@ -43,13 +51,15 @@ CREATE TABLE authorized_users (
   user_child_firstname VARCHAR NOT NULL,
   user_child_lastname VARCHAR NOT NULL
 );
+
 CREATE TABLE contacts (
   id SERIAL PRIMARY KEY,
   user_child_id INTEGER REFERENCES user_child(id),
-  office VARCHAR,
+  organization VARCHAR,
   fullname VARCHAR,
-  office_address VARCHAR,
-  phone INTEGER,
+  job_title VARCHAR,
+  org_address VARCHAR,
+  phone VARCHAR,
   website VARCHAR
 );
 
@@ -75,11 +85,21 @@ CREATE TABLE services_notes (
   timestamp VARCHAR NOT NULL
 );
 
-INSERT INTO users_main (first_name, last_name, relationship, username, email, password_digest)
-  VALUES ('Princess', 'Guerrero', 'mother', 'prinsesa', 'princess@princess.com', '$2a$10$f17jjX0NASQWYOln23Ogk.ePXm0TpAs2oq.k4.YOGQGTnkOvZlD/O');
+INSERT INTO users_main (username, email, password_digest)
+  VALUES ('prinsesa', 'princess@princess.com', '$2a$10$f17jjX0NASQWYOln23Ogk.ePXm0TpAs2oq.k4.YOGQGTnkOvZlD/O');
+
+INSERT INTO users_main_bio (first_name, last_name, relationship, pic, current_goals, next_steps, notes)
+  VALUES ('Princess', 'Guerrero', 'mother', 'mommy photo', 'ADL skills', 'transition to highschool', 'check-out his profile');
 
 INSERT INTO user_child (first_name, last_name, admin_id, date_of_birth, age, pic, school, grade, class_size, diagnosis, likes, dislikes)
   VALUES ('Philippe', 'Guerrero', 1, 'July 21, 2010', '12', 'photo', 'P4Q@Skillman', '7th', '6-1-1', 'Autism', 'iPad, running', 'loud noises, fireworks');
 
 INSERT INTO authorized_users (auth_user_firstname, auth_user_lastname, email, relationship, admin_id, admin_username, user_child_id , user_child_firstname, user_child_lastname)
   VALUES ('Joselito', 'Guerrero', 'jose@jose.com', 'father', 1, 'prinsesa', 1, 'Philippe', 'Guerrero');
+
+INSERT INTO contacts (user_child_id, organization, fullname, job_title, org_address, phone, website)
+  VALUES (1, 'The Thrive Network', 'Chad Alexander', 'Medicaid Service Coordinator', '241 37th St., Suite 604 Brooklyn, NY 11232', '(718) 965-1998', 'www.thethrivenetwork.org'),
+         (1, 'Marie Pense Center', 'Mildred Amarteifio', 'SETSS provider', '37 West 20th St., Suite 909, New York, NY 10011', '(212) 362-7013', 'mpcny.org');
+
+INSERT INTO services (user_child_id, provider_id, provider_fullname, frequency)
+  VALUES (1, 2, 'Mildred Amarteifio', '10 hours/week');
--- backend/db/queries.js
+++ backend/db/queries.js
@@ -25,20 +25,95 @@ function registerUser(req, res, next) {
 }
 
 // add main user bio
-// register a user child
-
-// get user
-function getUser(req, res, next) {
+function mainUserBio(req, res, next) {
   db
-    .one("SELECT username, email FROM users WHERE username=${username}", {
-      username: req.user.username
+    .none(
+      "INSERT INTO users_main_bio (first_name, last_name, relationship, pic, current_goals, next_steps, notes) VALUES (DEFAULT, ${first_name}, ${last_name}, ${relationship}, ${pic}, ${current_goals}, ${next_steps}, ${notes})",
+      {
+        first_name: req.body.username,
+        last_name: req.body.last_name,
+        relationship: req.body.relationship,
+        pic: req.body.pic,
+        current_goals: req.body.current_goals,
+        next_steps: req.body.next_steps,
+        notes: req.body.notes
+      }
+    )
+    .then(() => {
+      res.status(200).send("added main user bio into database");
+    })
+    .catch(err => {
+      console.log(`error adding main user bio: `, err);
+      // res.status(500).send("error adding user attributes: ", err);
+    });
+}
+
+// register a user child
+function registerUserChild(req, res, next) {
+  db
+    .none(
+      "INSERT INTO user_child (first_name, last_name, admin_id, date_of_birth, age, pic, school, grade, class_size, diagnosis, likes, dislikes) VALUES (DEFAULT, ${first_name}, ${last_name}, ${admin_id}, ${date_of_birth}, ${age}, ${pic}, ${school}, ${grade}, ${class_size}, ${diagnosis}, ${likes}, ${dislikes})",
+      {
+        first_name: req.body.username,
+        last_name: req.body.last_name,
+        admin_id: req.user_main.id,
+        date_of_birth: req.body.date_of_birth,
+        age: req.body.age,
+        pic: req.body.pic,
+        school: req.body.school,
+        grade: req.body.grade,
+        class_size: req.body.class_size,
+        diagnosis: req.body.diagnosis,
+        likes: req.body.likes,
+        dislikes: req.body.dislikes
+      }
+    )
+    .then(() => {
+      res.status(200).send("added child user bio into database");
+    })
+    .catch(err => {
+      console.log(`error adding child user bio: `, err);
+      // res.status(500).send("error adding user attributes: ", err);
+    });
+}
+
+// get main user
+function getMainUser(req, res, next) {
+  db
+    .one("SELECT username, email FROM users_main WHERE username=${username}", {
+      username: req.user_main.username
     })
     .then(data => {
       res.status(200).json({ user: data });
     });
 }
 
-// get a user child
+// get all authorized users
+function getAuthorizedUsers(req, res, next) {
+    db.any("SELECT id, auth_user_firstname, auth_user_lastname, email, relationship FROM authorized_users")
+    .then(data => {
+      console.log("data:", data);
+      res.status(200).json({
+        status: "success",
+        data: data,
+        message: "Retrieved all authorized users"
+      });
+    })
+    .catch(err => {
+      return next(err);
+    });
+}
+
+// get a user's child
+function getUserChild(req, res, next) {
+    db.one("SELECT first_name, last_name FROM user_child WHERE admin_id=${admin_id}", {
+        admin_id: req.user_child.admin_id
+    })
+    .then(data => {
+        res.status(200).json({ user: data });
+    });
+}
+
 
 // Log out user
 function logoutUser(req, res, next) {
@@ -49,7 +124,11 @@ function logoutUser(req, res, next) {
 // edit functions
 
 module.exports = {
-    registerUser,
-    getUser,
-    logoutUser
-}
+  registerUser,
+  mainUserBio,
+  registerUserChild,
+  getMainUser,
+  getAuthorizedUsers,
+  getUserChild,
+  logoutUser
+};
