ecdb4e13804aeda62aecbb96fd458a16cb1c7e81
princessguerrero <princessguerrero@ac.c4q.nyc>
2018-06-18 15:49:13 -0400

    heroku-version edit app.js

--- backend/app.js
+++ backend/app.js
@@ -29,10 +29,10 @@ app.use(passport.session());
 // app.use(express.static(path.join(__dirname, 'public')));
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.use('/', index);
